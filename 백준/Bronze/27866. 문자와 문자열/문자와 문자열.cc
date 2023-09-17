d7128393512da8775b17b06a3cd5d0ce9ded653b
DABIN LEE <81626630+dabinchiii@users.noreply.github.com>
2023-09-17 14:49:22 +0900

    [Bronze V] Title: 문자와 문자열, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Bronze/27866. 문자와 문자열/문자와 문자열.cc
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    string s;
+    int i;
+    cin >> s >> i;
+    cout << s[i-1];
+
+    return 0;
+}
