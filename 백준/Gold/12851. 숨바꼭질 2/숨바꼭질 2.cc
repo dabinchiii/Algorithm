1bcc0faa24b6b9bb5b560e1783e9fa542694e790
DABIN LEE <81626630+dabinchiii@users.noreply.github.com>
2023-10-23 00:30:56 +0900

    [Gold IV] Title: 숨바꼭질 2, Time: 68 ms, Memory: 9540 KB -BaekjoonHub

--- 백준/Gold/12851. 숨바꼭질 2/숨바꼭질 2.cc
+++ 백준/Gold/12851. 숨바꼭질 2/숨바꼭질 2.cc
@@ -18,29 +18,31 @@ void bfs(){
         q.pop();
 
         visited[currPos] = true;
-        if(isFound && AnswerT == currT && currPos == k){
-            AnswerCnt++;
-        }
+
         if(!isFound && currPos == k){
-            AnswerT = currT;
             isFound = true;
+            AnswerT = currT;
+        }
+        if(isFound && currPos == k && currT == AnswerT){
             AnswerCnt++;
         }
 
         int left = currPos - 1;
         int right = currPos + 1;
         int warp = 2 * currPos;
-        if(right < MAX && !visited[right]){
-            q.push({right, currT + 1});
-        }
+
         if(left >= 0 && !visited[left]){
             q.push({left, currT + 1});
+        }
+        if(right < MAX && !visited[right]){
+            q.push({right, currT + 1});
         }
         if(warp < MAX && !visited[warp]){
             q.push({warp, currT + 1});
         }
     }
 }
+
 int main(){
     cin >> n >> k;
 
