e6c252b9f03e2a86fae26e636a63f61ba9ff50e6
DABIN LEE <81626630+dabinchiii@users.noreply.github.com>
2023-10-23 00:22:07 +0900

    [Gold IV] Title: 숨바꼭질 2, Time: 72 ms, Memory: 9540 KB -BaekjoonHub

---
+++ 백준/Gold/12851. 숨바꼭질 2/숨바꼭질 2.cc
@@ -0,0 +1,51 @@
+#include <bits/stdc++.h>
+#define MAX 200000
+using namespace std;
+
+int n, k;
+bool isFound = false;
+vector<bool> visited(MAX);
+int AnswerT, AnswerCnt;
+
+void bfs(){
+    queue<pair<int, int>> q;
+    q.push({n, 0});
+    visited[n] = true;
+
+    while(!q.empty()){
+        int currPos = q.front().first;
+        int currT = q.front().second;
+        q.pop();
+
+        visited[currPos] = true;
+        if(isFound && AnswerT == currT && currPos == k){
+            AnswerCnt++;
+        }
+        if(!isFound && currPos == k){
+            AnswerT = currT;
+            isFound = true;
+            AnswerCnt++;
+        }
+
+        int left = currPos - 1;
+        int right = currPos + 1;
+        int warp = 2 * currPos;
+        if(right < MAX && !visited[right]){
+            q.push({right, currT + 1});
+        }
+        if(left >= 0 && !visited[left]){
+            q.push({left, currT + 1});
+        }
+        if(warp < MAX && !visited[warp]){
+            q.push({warp, currT + 1});
+        }
+    }
+}
+int main(){
+    cin >> n >> k;
+
+    bfs();
+    cout << AnswerT << ' ' << AnswerCnt;
+
+    return 0;
+}
