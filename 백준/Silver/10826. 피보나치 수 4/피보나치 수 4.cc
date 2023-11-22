9580a5d140d9520ab44073b4f7c7f7c234859e25
DABIN LEE <81626630+dabinchiii@users.noreply.github.com>
2023-11-22 20:34:55 +0900

    [Silver V] Title: 피보나치 수 4, Time: 272 ms, Memory: 13684 KB -BaekjoonHub

---
+++ 백준/Silver/10826. 피보나치 수 4/피보나치 수 4.cc
@@ -0,0 +1,47 @@
+#include <bits/stdc++.h>
+#define MAX_N 10000
+
+using namespace std;
+
+string dp[MAX_N + 1];
+
+void add(int pos, string a, string b){
+    while(a.length() < b.length()) a += '0';
+    while(a.length() > b.length()) b += '0';
+
+    string result = "";
+    int curr, carry = 0, value;
+    for(int i=0; i<a.length(); i++){
+        curr = carry + (a[i] - '0') + (b[i] - '0');
+        carry = curr / 10;
+        value = curr % 10;
+
+        result += to_string(value);
+    }
+
+    if(carry) result += "1";
+
+    dp[pos] = result;
+}
+
+int main(){
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    int n;
+    cin >> n;
+
+
+    dp[0] = "0";
+    dp[1] = "1";
+    for(int i=2; i<=n; i++){
+        add(i, dp[i-1], dp[i-2]);
+    }
+
+    string Answer = dp[n];
+    reverse(Answer.begin(), Answer.end());
+
+    cout << Answer;
+
+    return 0;
+}
