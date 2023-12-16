cd84cb6dc9dd0fb6f997d8b3fb87042109614014
DABIN LEE <81626630+dabinchiii@users.noreply.github.com>
2023-12-16 23:50:01 +0900

    [Bronze V] Title: Plane, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/8370. Plane/Plane.cc
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main(){
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    int n1, n2, k1, k2;
+    cin >> n1 >> k1 >> n2 >> k2;
+    cout << k1 * n1 + k2 * n2;
+
+    return 0;
+}
