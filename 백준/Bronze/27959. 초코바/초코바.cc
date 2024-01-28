39b7dba09a7b653322e330f30eb4d938c9495ebd
DABIN LEE <81626630+dabinchiii@users.noreply.github.com>
2024-01-28 18:22:26 +0900

    [Bronze V] Title: 초코바, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/27959. 초코바/초코바.cc
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main(){
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    int n, m;
+    cin >> n >> m;
+    cout << (n * 100 >= m ? "Yes" : "No");
+
+    return 0;
+}
