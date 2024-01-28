7f5660819f8f516b607b02d377e48e53b9c88aeb
DABIN LEE <81626630+dabinchiii@users.noreply.github.com>
2024-01-28 18:20:21 +0900

    [Bronze V] Title: Can you add this?, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/7891. Can you add this？/Can you add this？.cc
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main(){
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    int T;
+    cin >> T;
+    for(int t=0; t<T; t++){
+        int a, b;
+        cin >> a >> b;
+        cout << a + b << '\n';
+    }
+
+    return 0;
+}
