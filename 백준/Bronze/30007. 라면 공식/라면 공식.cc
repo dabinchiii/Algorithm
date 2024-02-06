b6a69e5515c47339fc23b314bf80575f297f8784
DABIN LEE <81626630+dabinchiii@users.noreply.github.com>
2024-02-07 05:40:29 +0900

    [Bronze V] Title: 라면 공식, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/30007. 라면 공식/라면 공식.cc
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main(){
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    int n;
+    cin >> n;
+    for(int i=0; i<n; i++){
+        int a, b, x;
+        cin >> a >> b >> x;
+        cout << a * (x - 1) + b << '\n';
+    }
+
+    return 0;
+}
