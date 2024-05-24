18a4b5aa865c9129e27e47df07cd8e3aec76585c
DABIN LEE <81626630+dabinchiii@users.noreply.github.com>
2024-05-24 18:12:36 +0900

    [Silver III] Title: N과 M (2), Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/15650. N과 M （2）/N과 M （2）.cc
@@ -0,0 +1,33 @@
+#include <bits/stdc++.h>
+#define MAX_M 8
+
+using namespace std;
+
+int n, m;
+int ans[MAX_M];
+
+void bt(int cnt, int left){
+    if(cnt == m){
+        for(int i=0; i<m; i++) cout << ans[i] << ' ';
+        cout << '\n';
+        return;
+    }
+
+    for(int i=left; i<=n; i++){
+        ans[cnt] = i;
+        bt(cnt + 1, i + 1);
+    }
+
+    return;
+}
+
+int main(){
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    cin >> n >> m;
+
+    bt(0, 1);
+
+    return 0;
+}
