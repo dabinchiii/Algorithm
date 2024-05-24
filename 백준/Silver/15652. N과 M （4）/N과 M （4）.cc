1aabea032c242c62609d291b3d5900fddc0a78b0
DABIN LEE <81626630+dabinchiii@users.noreply.github.com>
2024-05-24 18:24:43 +0900

    [Silver III] Title: N과 M (4), Time: 4 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Silver/15652. N과 M （4）/N과 M （4）.cc
+++ 백준/Silver/15652. N과 M （4）/N과 M （4）.cc
@@ -1,34 +1,33 @@
 #include <bits/stdc++.h>
+#define MAX_M 8
 
 using namespace std;
 
 int n, m;
-int arr[9];
+int ans[MAX_M];
 
-void f(int k){
-    if(k == m){
-        for(int i=0; i<m; ++i)
-            cout << arr[i] << ' ';
+void bt(int cnt, int left){
+    if(cnt == m){
+        for(int i=0; i<m; i++){
+            cout << ans[i] << ' ';
+        }
         cout << '\n';
         return;
     }
 
-    int num = arr[k-1];
-    if(k==0) num = 1;
-    for(int i=num; i<=n; i++){
-        arr[k] = i;
-        f(k+1);
+    for(int i=left; i<=n; i++){
+        ans[cnt] = i;
+        bt(cnt + 1, i);
     }
-
 }
 
 int main(){
     ios_base::sync_with_stdio(false);
-    cin.tie(NULL); cout.tie(NULL);
+    cin.tie(NULL);
 
     cin >> n >> m;
 
-    f(0);
+    bt(0, 1);
 
     return 0;
 }
