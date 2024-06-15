e87a1f470e5d15fd1c9b2c17caca0c25c05b7607
DABIN LEE <81626630+dabinchiii@users.noreply.github.com>
2024-06-15 10:50:58 +0900

    [Gold IV] Title: 스도쿠, Time: 80 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Gold/2580. 스도쿠/스도쿠.cc
@@ -0,0 +1,83 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int board[9][9], ans[9][9];
+bool row[9][10], col[9][10], box[3][3][10];
+
+pair<int, int> findNextBlank(int x, int y){
+    int currX = x, currY = y;
+
+    while(board[currX][currY] != 0){
+        currY++;
+        if(currY == 9){
+            currY = 0;
+            currX++;
+        }
+        if(currX == 9){
+            return {-1, -1};
+        }
+    }
+
+    return {currX, currY};
+}
+
+void bt(int x, int y){
+    if(ans[0][0] != 0) return;
+    if(x == -1){
+        for(int i=0; i<9; i++){
+            for(int j=0; j<9; j++){
+                ans[i][j] = board[i][j];
+            }
+        }
+
+        return;
+    }
+
+    board[x][y] = -1;
+    pair<int, int> nxt = findNextBlank(x, y);
+    board[x][y] = 0;
+
+    for(int i=1; i<=9; i++){
+        if(row[x][i]) continue;
+        if(col[y][i]) continue;
+        if(box[x/3][y/3][i]) continue;
+
+        board[x][y] = i;
+        row[x][i] = col[y][i] = box[x/3][y/3][i] = true;
+
+        bt(nxt.first, nxt.second);
+
+        board[x][y] = 0;
+        row[x][i] = col[y][i] = box[x/3][y/3][i] = false;
+    }
+
+    return;
+}
+
+int main(){
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    for(int i=0; i<9; i++){
+        for(int j=0; j<9; j++){
+            cin >> board[i][j];
+
+            row[i][board[i][j]] = true;
+            col[j][board[i][j]] = true;
+            box[i/3][j/3][board[i][j]] = true;
+        }
+    }
+
+    pair<int, int> start = findNextBlank(0, 0);
+    bt(start.first, start.second);
+
+    for(int i=0; i<9; i++){
+        for(int j=0; j<9; j++){
+            cout << ans[i][j] << ' ';
+        }
+        cout << '\n';
+    }
+
+    return 0;
+}
