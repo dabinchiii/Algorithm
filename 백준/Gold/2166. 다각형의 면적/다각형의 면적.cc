31bbd800ea0f362ea798e7f45bb819354a420a86
DABIN LEE <81626630+dabinchiii@users.noreply.github.com>
2024-06-24 18:32:59 +0900

    [Gold V] Title: 다각형의 면적, Time: 4 ms, Memory: 2916 KB -BaekjoonHub

---
+++ 백준/Gold/2166. 다각형의 면적/다각형의 면적.cc
@@ -0,0 +1,40 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+struct coord{
+    long double x, y;
+};
+
+int n;
+vector<struct coord> v;
+long double ans;
+
+int main(){
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    cin >> n;
+
+    long double x, y;
+    for(int i=0; i<n; i++){
+        cin >> x >> y;
+        v.push_back({x, y});
+    }
+
+    long double sumA, sumB;
+    for(int i=1; i<n-1; i++){
+        sumA = v[0].x * v[i].y + v[i].x * v[i+1].y + v[i+1].x * v[0].y;
+        sumB = v[0].x * v[i+1].y + v[i].x * v[0].y + v[i+1].x * v[i].y;
+
+        ans += (sumA - sumB) / 2;
+    }
+    //ans = abs(ans);
+
+    cout << fixed;
+    cout.precision(1);
+
+    cout << abs(ans);
+
+    return 0;
+}
