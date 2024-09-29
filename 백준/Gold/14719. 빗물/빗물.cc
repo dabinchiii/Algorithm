c08ee753941bdf16b0aebc4164ec028a1bfaab1e
DABIN LEE <81626630+dabinchiii@users.noreply.github.com>
2024-09-30 04:34:26 +0900

    [Gold V] Title: 빗물, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

--- 백준/Gold/14719. 빗물/빗물.cc
+++ 백준/Gold/14719. 빗물/빗물.cc
@@ -4,7 +4,6 @@ using namespace std;
 
 int h, w;
 int block[500];
-int water[500];
 
 int main(){
     ios_base::sync_with_stdio(false);
@@ -15,33 +14,14 @@ int main(){
         cin >> block[i];
     }
 
-    for(int i=0; i<w; i++){
-        for(int j=i + 1; j<w; j++){
-            if(block[j] < block[i]) continue;
-
-            for(int k=i + 1; k<j; k++){
-                water[k] = max(water[k], block[i] - block[k]);
-            }
-
-            break;
-        }
-    }
-
-    for(int i=w-1; i>=0; i--){
-        for(int j=i - 1; j>=0; j--){
-            if(block[j] < block[i]) continue;
-
-            for(int k=i - 1; k>j; k--){
-                water[k] = max(water[k], block[i] - block[k]);
-            }
-
-            break;
-        }
-    }
-
     int ans = 0;
-    for(int i=0; i<w; i++){
-        ans += water[i];
+    for(int i=1; i<w - 1; i++){
+        int leftHigh = 0, rightHigh = 0;
+
+        for(int j=i - 1; j>=0; j--) leftHigh = max(leftHigh, block[j]);
+        for(int j=i + 1; j<w; j++) rightHigh = max(rightHigh, block[j]);
+
+        ans += max(0, min(leftHigh, rightHigh) - block[i]);
     }
 
     cout << ans;
