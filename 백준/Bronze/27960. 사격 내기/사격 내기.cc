bd27353c9160d5a7a66fc94b9bcaf38b6da8cf4c
DABIN LEE <81626630+dabinchiii@users.noreply.github.com>
2025-03-24 03:55:47 +0900

    [Bronze III] Title: 사격 내기, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Bronze/27960. 사격 내기/사격 내기.cc
+++ 백준/Bronze/27960. 사격 내기/사격 내기.cc
@@ -1,24 +1,12 @@
 #include <iostream>
-#include <bitset>
-#include <typeinfo>
 
 using namespace std;
 
-typedef bitset<10> bit;
-
-int sumA, sumB;
-int ans;
-
-void solve(){
-    ans = static_cast<int>((bit(sumA) ^ bit(sumB)).to_ulong());
-    
-    return;
-}
-
 int main(){
-    cin >> sumA >> sumB;
-    solve();
-    cout << ans;
-
+    int a, b;
+    cin >> a >> b;
+    
+    cout << (a ^ b);
+    
     return 0;
 }
