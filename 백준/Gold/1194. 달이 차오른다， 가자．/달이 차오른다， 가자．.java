1a7c18982b1e0b8bf7cac58f9598372e0be58781
DABIN LEE <81626630+dabinchiii@users.noreply.github.com>
2025-08-07 16:47:54 +0900

    [Gold I] Title: 달이 차오른다, 가자., Time: 168 ms, Memory: 19272 KB -BaekjoonHub

---
+++ 백준/Gold/1194. 달이 차오른다， 가자．/달이 차오른다， 가자．.java
@@ -0,0 +1,113 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.StringTokenizer;
+
+public class Main {
+	static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+	static StringTokenizer st;
+	
+	static int n, m;
+	static char[][] board;
+	static int ans;
+	
+	static int sr, sc;
+	
+	static int[] dr = {-1, 1, 0, 0};
+	static int[] dc = {0, 0, -1, 1};
+	
+	static int[] keyValue = {1, 10, 100, 1000, 10000, 100000, 1000000};
+	
+	public static void main(String[] args) throws IOException{
+		st = new StringTokenizer(br.readLine());
+		n = Integer.parseInt(st.nextToken());
+		m = Integer.parseInt(st.nextToken());
+		
+		board = new char[n][m];
+		
+		for(int i=0; i<n; i++) {
+			String line = br.readLine();
+			for(int j=0; j<m; j++) {
+				board[i][j] = line.charAt(j);
+				
+				if(board[i][j] == '0') { // 민식이 서 있는 곳
+					sr = i;
+					sc = j;
+				}
+				
+			}
+		}
+		
+		solve();
+		
+		System.out.println(ans);
+	}
+
+	private static void solve() {
+		ans = -1;
+		
+		Queue<int[]> que = new LinkedList<>();
+		ArrayList<Integer>[][] visited = new ArrayList[n][m];
+		
+		for(int i=0; i<n; i++) {
+			for(int j=0; j<m; j++) {
+				visited[i][j] = new ArrayList<>();
+			}
+		}
+		
+		que.offer(new int[] {sr, sc, 0, 0});
+		visited[sr][sc].add(0);
+		
+		while(!que.isEmpty()) {
+			int[] curr = que.poll();
+			int r = curr[0];
+			int c = curr[1];
+			int t = curr[2];
+			int k = curr[3];
+			
+			if(board[r][c] == '1') {
+				ans = t;
+				return;
+			}
+			
+			for(int d=0; d<4; d++) {
+				int nr = r + dr[d];
+				int nc = c + dc[d];
+				int nt = t + 1;
+				int nk = k;
+				
+				if(!check(nr, nc)) continue;
+				if(board[nr][nc] == '#') continue;
+				
+				if(board[nr][nc] >= 'A' && board[nr][nc] <= 'Z') { // 문
+					int idx = board[nr][nc] - 'A';
+					if(!hasKey(k, idx)) continue; // 열쇠없음
+				}
+				if(board[nr][nc] >= 'a' && board[nr][nc] <= 'f') { // 열쇠
+					int idx = board[nr][nc] - 'a';
+					if(!hasKey(k, idx)) {
+						nk += keyValue[idx];
+					}
+				}
+				
+				if(visited[nr][nc].contains(nk)) continue;
+				
+				que.offer(new int[] {nr, nc, nt, nk});
+				visited[nr][nc].add(nk);
+			}
+		}
+		
+		return;
+	}
+	static boolean hasKey(int value, int idx) {
+		return (value / keyValue[idx]) % 10 == 1;
+	}
+	static boolean check(int r, int c) {
+		return r >= 0 && r < n && c >= 0 && c < m;
+	}
+	
+}
