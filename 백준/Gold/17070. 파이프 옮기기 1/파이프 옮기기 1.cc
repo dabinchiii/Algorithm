aa26310f5f56e9bea058d3ad92664d10d5911f26
DABIN LEE <81626630+dabinchiii@users.noreply.github.com>
2025-09-08 14:41:13 +0900

    [Gold V] Title: 파이프 옮기기 1, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Gold/17070. 파이프 옮기기 1/파이프 옮기기 1.cc
@@ -0,0 +1,45 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int N;
+int arr[17][17], dp[17][17][3];
+int ans;
+
+void solve() {
+	dp[1][2][0] = 1;
+	
+	for (int i = 1; i <= N; i++) {
+		for (int j = 1; j <= N; j++) {
+			if (arr[i][j] == 1) continue;
+
+			dp[i][j][0] += dp[i][j - 1][0] + dp[i][j - 1][1];
+
+			if (arr[i - 1][j] == 0 && arr[i][j - 1] == 0) {
+				dp[i][j][1] += dp[i - 1][j - 1][0] + dp[i - 1][j - 1][1] + dp[i - 1][j - 1][2];
+			}
+			
+			dp[i][j][2] += dp[i - 1][j][1] + dp[i - 1][j][2];
+		}
+	}
+
+	ans = dp[N][N][0] + dp[N][N][1] + dp[N][N][2];
+	
+	return;
+}
+
+int main() {
+	cin >> N;
+	for (int i = 1; i <= N; i++) {
+		for (int j = 1; j <= N; j++) {
+			cin >> arr[i][j];
+		}
+	}
+
+	solve();
+
+	cout << ans;
+
+	return 0;
+}
