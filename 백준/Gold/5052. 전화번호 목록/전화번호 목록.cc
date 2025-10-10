dd4e6ec0a0091f3c621ca19f7c6ce7d57a683336
DABIN LEE <81626630+dabinchiii@users.noreply.github.com>
2025-10-10 13:44:12 +0900

    [Gold IV] Title: 전화번호 목록, Time: 180 ms, Memory: 7796 KB -BaekjoonHub

--- 백준/Gold/5052. 전화번호 목록/전화번호 목록.cc
+++ 백준/Gold/5052. 전화번호 목록/전화번호 목록.cc
@@ -1,64 +1,76 @@
-#include <bits/stdc++.h>
+#include <iostream>
+#include <vector>
+#include <string>
+#include <algorithm>
 
 using namespace std;
 
-bool Answer;
+bool ans;
 
-struct TrieNode{
-    bool isEnd;
-    TrieNode* child[10];
+struct TrieNode {
+	bool finish;
+	TrieNode *node[10];
 
-    TrieNode(){
-        isEnd = false;
-        for(int i=0; i<10; i++) child[i] = NULL;
-    }
+	TrieNode() { // 생성자
+		finish = false;
+		for (int i = 0; i < 10; i++) node[i] = NULL;
+	}
+	~TrieNode() {
+		for (int i = 0; i < 10; i++) {
+			if (node[i]) delete node[i];
+		}
+	}
 
-    ~TrieNode() {
-        for(int i=0; i<10; i++){
-            if(child[i]) delete child[i];
-        }
-    }
+	void insert(string &str, int idx) {
+		if (idx == str.size()) {
+			finish = true;
+			return;
+		}
 
-    void insert(string& key, int index){
-        if(index == key.length()){
-            isEnd = true;
-            return;
-        }
+		int curr = str[idx] - '0';
+		if (node[curr] == NULL) {
+			node[curr] = new TrieNode();
+		}
+		else if (node[curr]->finish) { // 있으면 안되는데 있다.
+			ans = false;
+			return;
+		}
 
-        int next = key[index] - '0';
-        if(child[next] == nullptr){
-            child[next] = new TrieNode;
-        }
-        if(child[next]->isEnd) Answer = false;
-        child[next]->insert(key, index + 1);
+		node[curr]->insert(str, idx + 1);
 
-    }
+		return;
+	}
 };
 
-int main(){
-    ios_base::sync_with_stdio(false);
-    cin.tie(NULL);
+int main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
 
-    int T;
-    cin >> T;
-    for(int t=0; t<T; t++){
-        int n;
-        cin >> n;
+	int TC;
+	cin >> TC;
+	for (int tc = 1; tc <= TC; ++tc) {
+		int N;
+		cin >> N;
 
-        vector<string> arr(n);
-        for(int i=0; i<n; i++){
-            cin >> arr[i];
-        }
-        sort(arr.begin(), arr.end());
+		vector<string> arr;
+		string str;
+		for (int i = 0; i < N; i++) {
+			cin >> str;
+			arr.push_back(str);
+		}
 
-        Answer = true;
-        TrieNode root;
-        for(string number : arr){
-            root.insert(number, 0);
-        }
+		// 짧은 것부터 넣어야 되므로
+		sort(arr.begin(), arr.end());
 
-        cout << (Answer ? "YES" : "NO") << '\n';
-    }
+		TrieNode root;
+		ans = true;
+		for (string curr : arr) {
+			root.insert(curr, 0);
+			if (!ans) break;
+		}
 
-    return 0;
+		cout << (ans ? "YES" : "NO") << '\n';
+	}
+
+	return 0;
 }
