d0fb0fff707ad241e149392bd576dbde7b297843
DABIN LEE <81626630+dabinchiii@users.noreply.github.com>
2025-11-06 21:26:25 +0900

    [Silver IV] Title: 쿠키의 신체 측정, Time: 4 ms, Memory: 3004 KB -BaekjoonHub

---
+++ 백준/Silver/20125. 쿠키의 신체 측정/쿠키의 신체 측정.cc
@@ -0,0 +1,62 @@
+#include <iostream>
+
+using namespace std;
+
+struct loc{
+    int r, c;
+};
+
+int dr[] = {-1, 1, 0, 0};
+int dc[] = {0, 0, -1, 1};
+
+int N;
+bool board[1002][1002];
+
+loc findHeart(){
+    for(int i=2; i<N; i++){
+        for(int j=2; j<N; j++){
+            if(board[i][j] && board[i - 1][j] && board[i + 1][j] && board[i][j - 1] && board[i][j + 1]){
+                return {i, j};
+            }
+        }
+    }
+    return {-1, -1};
+}
+int cntLen(loc start, int d){
+    int nr = start.r, nc = start.c;
+    int cnt = 0;
+    while(true){
+        nr += dr[d];
+        nc += dc[d];
+        if(!board[nr][nc]) break;
+        ++cnt;
+    }
+    return cnt;
+}
+int main(){
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    
+    cin >> N;
+    
+    string line;
+    for(int i=1; i<=N; i++){
+        cin >> line;
+        
+        for(int j=1; j<=N; j++){
+            board[i][j] = line[j - 1] == '*';
+        }
+    }
+    
+    loc heart = findHeart();
+    int leftArm = cntLen(heart, 2);
+    int rightArm = cntLen(heart, 3);
+    int waist = cntLen(heart, 1);
+    int leftLeg = cntLen({heart.r + waist, heart.c - 1}, 1);
+    int rightLeg = cntLen({heart.r + waist, heart.c + 1}, 1);
+    
+    cout << heart.r << ' ' << heart.c << '\n';
+    cout << leftArm << ' ' << rightArm << ' ' << waist << ' ' << leftLeg << ' ' << rightLeg;
+    
+    return 0;
+}
