118d1bb059d86afcae1e6602d517a1c4b7c5e235
DABIN LEE <81626630+dabinchiii@users.noreply.github.com>
2025-11-07 00:24:04 +0900

    [Silver II] Title: 질문은 계속돼, Time: 52 ms, Memory: 3584 KB -BaekjoonHub

--- 백준/Silver/32194. 질문은 계속돼/질문은 계속돼.cc
+++ 백준/Silver/32194. 질문은 계속돼/질문은 계속돼.cc
@@ -4,50 +4,46 @@
 
 using namespace std;
 
+struct Node{
+    int lastX, lastO;
+};
+
 int N;
-int arr[MAX_N + 2][2];
+Node arr[MAX_N + 2];
 
 int main(){
     ios_base::sync_with_stdio(false);
-    cin.tie(NULL);
+    cin.tie(NULL); cout.tie(NULL);
     
     cin >> N;
     
-    arr[1][0] = 0; // 가장 최근 X
-    arr[1][1] = 1; // 가장 최근 O
+    arr[1] = {0, 1};
     
-    string ans = "";
     int comm, x, y;
     for(int i=2; i<=N + 1; i++){
         cin >> comm >> x >> y;
         
         if(comm == 1){ // x~y 모두 맞았냐?
-            if(arr[y][0] < x){ // 그렇다 = y 시점에서 가장 최근 틀린 것이 x보다 이전이다.
-                ans += "Yes\n";
-                arr[i][0] = arr[i - 1][0];
-                arr[i][1] = i;
+            if(arr[y].lastX < x){ // 그렇다 = y 시점에서 가장 최근 틀린 것이 x보다 이전이다.
+                cout << "Yes\n";
+                arr[i] = {arr[i - 1].lastX, i};
             }
             else{
-                ans += "No\n";
-                arr[i][0] = i;
-                arr[i][1] = arr[i - 1][1];
+                cout << "No\n";
+                arr[i] = {i, arr[i - 1].lastO};
             }
         }
         else{ // x~y 모두 틀렸냐?
-            if(arr[y][1] < x){ // 그렇다 = y 시점에서 가장 최근 맞은 것이 x보다 이전이다.
-                ans += "Yes\n";
-                arr[i][0] = arr[i - 1][0];
-                arr[i][1] = i;
+            if(arr[y].lastO < x){ // 그렇다 = y 시점에서 가장 최근 맞은 것이 x보다 이전이다.
+                cout << "Yes\n";
+                arr[i] = {arr[i - 1].lastX, i};
             }
             else{ // 아니다
-                ans += "No\n";
-                arr[i][0] = i;
-                arr[i][1] = arr[i - 1][1];
+                cout << "No\n";
+                arr[i] = {i, arr[i - 1].lastO};
             }
         }
     }
-    
-    cout << ans;
     
     return 0;
 }
