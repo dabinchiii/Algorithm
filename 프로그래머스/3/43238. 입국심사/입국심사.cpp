4e4d9931631ed9e4ed90e90dc2bbd84555dd5c33
DABIN LEE <81626630+dabinchiii@users.noreply.github.com>
2025-12-06 19:53:57 +0900

    [level 3] Title: 입국심사, Time: 55.78 ms, Memory: 7.07 MB -BaekjoonHub

---
+++ 프로그래머스/3/43238. 입국심사/입국심사.cpp
@@ -0,0 +1,33 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int N;
+vector<int> times;
+
+bool check(long long t){
+    long long cnt = 0;
+    for(int curr : times) cnt += t / curr;
+    return cnt >= N;
+}
+
+long long solution(int n, vector<int> _times) {
+    N = n;
+    times = _times;
+    
+    long long left = 1, right = 1000000000000000000;
+    long long ans = right;
+    while(left <= right){
+        long long mid = left + ((right - left) / 2);
+        if(check(mid)){
+            ans = mid;
+            right = mid - 1;
+        }
+        else{
+            left = mid + 1;
+        }
+    }
+    
+    return ans;
+}
