cc53542d1813de7e413e27b7cde421052afbf1da
DABIN LEE <81626630+dabinchiii@users.noreply.github.com>
2026-01-11 00:13:40 +0900

    [Silver IV] Title: 토너먼트, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/1057. 토너먼트/토너먼트.cc
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    int N, a, b;
+    cin >> N >> a >> b;
+    --a; --b;
+    
+    int cnt = 0;
+    while(a >> cnt != b >> cnt) ++cnt;
+    
+    cout << cnt;
+    
+    return 0;
+}
