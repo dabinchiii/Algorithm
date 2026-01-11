6ffbb28de4d7b1bce53b0828585b06f5cadb916a
DABIN LEE <81626630+dabinchiii@users.noreply.github.com>
2026-01-11 14:25:38 +0900

    [Gold III] Title: 개구리 점프, Time: 68 ms, Memory: 4848 KB, Score: 100 point -BaekjoonHub

--- 백준/Gold/17619. 개구리 점프/개구리 점프.cc
+++ 백준/Gold/17619. 개구리 점프/개구리 점프.cc
@@ -7,8 +7,7 @@
 using namespace std;
 
 struct wood{
-    int id;
-    int x1, x2, y;
+    int id, x1, x2;
 };
 
 bool cmp(wood &a, wood &b){
@@ -35,16 +34,21 @@ void makeUnion(int a, int b){
     parent[rootA] = rootB;
     return;
 }
-bool isUnion(int a, int b){
-    return findRoot(a) == findRoot(b);
-}
 
 void solve(){
     sort(v.begin(), v.end(), cmp);
     makeSet();
     
-    for(int i=0; i<N - 1; i++){
-        if(v[i].x2 >= v[i + 1].x1) makeUnion(v[i].id, v[i + 1].id);
+    int e = -1, eid = -1;
+    for(int i=0; i<N; i++){
+        if(v[i].x1 <= e){ // 계속 연결됨
+            makeUnion(v[i].id, eid);
+            e = max(e, v[i].x2);
+        }
+        else{ // 끊김
+            eid = v[i].id;
+            e = v[i].x2;
+        }
     }
     
     return;
@@ -59,7 +63,7 @@ int main(){
     int x1, x2, y;
     for(int i=1; i<=N; i++){
         cin >> x1 >> x2 >> y;
-        v.push_back({i, x1, x2, y});
+        v.push_back({i, x1, x2});
     }
     
     solve();
@@ -67,7 +71,7 @@ int main(){
     int a, b;
     for(int q=0; q<Q; q++){
         cin >> a >> b;
-        cout << (isUnion(a, b) ? 1 : 0) << '\n';
+        cout << (findRoot(a) == findRoot(b) ? 1 : 0) << '\n';
     }
     
     return 0;
