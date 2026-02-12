edafcda5d7314edb7eb296e80c1ec19b5528fc9f
DABIN LEE <81626630+dabinchiii@users.noreply.github.com>
2026-02-12 12:56:29 +0900

    [level 2] Title: 고양이와 개는 몇 마리 있을까, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

---
+++ 프로그래머스/2/59040. 고양이와 개는 몇 마리 있을까/고양이와 개는 몇 마리 있을까.sql
@@ -0,0 +1,9 @@
+# 고양이와 개가 각각 몇 마리인지 조회
+# 고양이를 개보다 먼저 조회
+SELECT
+    ANIMAL_TYPE,
+    COUNT(*) AS 'count'
+FROM ANIMAL_INS
+WHERE ANIMAL_TYPE IN ('Cat', 'Dog')
+GROUP BY ANIMAL_TYPE
+ORDER BY FIELD(ANIMAL_TYPE, 'Cat', 'Dog');
