fa4c74cdd14982ffd1f55cf4f617a9596779b030
DABIN LEE <81626630+dabinchiii@users.noreply.github.com>
2026-02-12 14:47:33 +0900

    [level 2] Title: ROOT 아이템 구하기, Time: , Memory: undefined -BaekjoonHub

---
+++ 프로그래머스/2/273710. ROOT 아이템 구하기/ROOT 아이템 구하기.sql
@@ -0,0 +1,9 @@
+# ROOT 아이템을 찾아 아이템 ID(ITEM_ID), 아이템 명(ITEM_NAME)을 출력
+# 결과는 아이템 ID를 기준으로 오름차순 정렬
+SELECT
+    I.ITEM_ID,
+    I.ITEM_NAME
+FROM ITEM_INFO I JOIN ITEM_TREE T
+    ON I.ITEM_ID = T.ITEM_ID
+WHERE T.PARENT_ITEM_ID IS NULL
+ORDER BY I.ITEM_ID;
