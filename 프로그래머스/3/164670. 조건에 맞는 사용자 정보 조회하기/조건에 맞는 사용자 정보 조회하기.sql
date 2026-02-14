0b8a200c222a3b6a325272c3ba42bef32852900e
DABIN LEE <81626630+dabinchiii@users.noreply.github.com>
2026-02-14 17:10:32 +0900

    [level 3] Title: 조건에 맞는 사용자 정보 조회하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

---
+++ 프로그래머스/3/164670. 조건에 맞는 사용자 정보 조회하기/조건에 맞는 사용자 정보 조회하기.sql
@@ -0,0 +1,28 @@
+# 중고 거래 게시물을 3건 이상 등록한 사용자의
+# 사용자 ID, 닉네임, 전체주소, 전화번호를 조회
+# 전체 주소는 시, 도로명 주소, 상세 주소가 함께 출력되도록 해주시고,
+# 전화번호의 경우 xxx-xxxx-xxxx 같은 형태로 하이픈 문자열(-)을 삽입하여 출력
+# 결과는 회원 ID를 기준으로 내림차순 정렬
+SELECT
+    U.USER_ID,
+    U.NICKNAME,
+    CONCAT(
+        U.CITY, ' ',
+        U.STREET_ADDRESS1, ' ',
+        U.STREET_ADDRESS2
+    ) AS '전체주소',
+    CONCAT(
+        SUBSTR(U.TLNO, 1, 3), '-',
+        SUBSTR(U.TLNO, 4, 4), '-',
+        SUBSTR(U.TLNO, 8, 4)
+    ) AS '전화번호'
+FROM USED_GOODS_USER U
+JOIN (
+    SELECT 
+        WRITER_ID AS USER_ID,
+        COUNT(*) AS CNT
+    FROM USED_GOODS_BOARD
+    GROUP BY WRITER_ID
+) BOARD ON U.USER_ID = BOARD.USER_ID
+WHERE BOARD.CNT >= 3
+ORDER BY U.USER_ID DESC;
