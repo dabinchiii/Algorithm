07420ff0aeee879ba577cca64bc253b17cfb6f94
DABIN LEE <81626630+dabinchiii@users.noreply.github.com>
2026-02-14 22:09:09 +0900

    Update 물고기 종류 별 대어 찾기.sql

--- 프로그래머스/3/293261. 물고기 종류 별 대어 찾기/물고기 종류 별 대어 찾기.sql
+++ 프로그래머스/3/293261. 물고기 종류 별 대어 찾기/물고기 종류 별 대어 찾기.sql
@@ -4,26 +4,20 @@
 # 잡은 물고기의 길이가 10cm 이하일 경우에는 LENGTH 가 NULL
 # 물고기 종류별 가장 큰 물고기는 1마리만 있으며 10cm 이하의 물고기가 가장 큰 경우는 없습니다.
 SELECT
-    L.ID,
+    I.ID,
     N.FISH_NAME,
-    L.LENGTH
+    I.LENGTH
 FROM (
     SELECT
         ID,
         FISH_TYPE,
-        LENGTH
-    FROM (
-        SELECT
-            ID,
-            FISH_TYPE,
-            LENGTH,
-            RANK() OVER (
-                PARTITION BY FISH_TYPE
-                ORDER BY LENGTH DESC
-            ) AS R
-        FROM FISH_INFO
-    ) T
-    WHERE R = 1
-) L JOIN FISH_NAME_INFO N
-    ON L.FISH_TYPE = N.FISH_TYPE
-ORDER BY L.ID ASC;
+        LENGTH,
+        RANK() OVER (
+            PARTITION BY FISH_TYPE
+            ORDER BY LENGTH DESC
+        ) AS R
+    FROM FISH_INFO
+) I JOIN FISH_NAME_INFO N
+    ON I.FISH_TYPE = N.FISH_TYPE
+WHERE R = 1
+ORDER BY I.ID ASC;
