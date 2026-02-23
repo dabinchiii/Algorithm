fa36dc5ff6da70b29c0758313ee5e272323083c1
DABIN LEE <81626630+dabinchiii@users.noreply.github.com>
2026-02-23 23:57:56 +0900

    [Gold V] Title: 하노이 탑, Time: 108 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Gold/1914. 하노이 탑/하노이 탑.cc
+++ 백준/Gold/1914. 하노이 탑/하노이 탑.cc
@@ -7,33 +7,36 @@ using namespace std;
 
 void hanoi(int from, int tmp, int to, int curr){
     if(curr == 1){
-        printf("%d %d\n", from, to);
+        cout << from << ' ' << to << '\n';
         return;
     }
     
     hanoi(from, to, tmp, curr - 1);
-    printf("%d %d\n", from, to);
+    cout << from << ' ' << to << '\n';
     hanoi(tmp, from, to, curr - 1);
     
     return;
 }
 
 string my2Pow(int N){
-    string res = "1";
-    int carry = 0, sum;
+    string res = "1", tmp;
+    int carry, curr;
+    
+    // res에 2^N의 리버스 저장
+    carry = 0;
     for(int i=0; i<N; i++){
-        string tmp = "";
+        tmp = "";
         
         for(int j=0; j<res.size(); j++){
-            sum = ((res[j] - '0') << 1) + carry;
+            curr = ((res[j] - '0') << 1) + carry;
             carry = 0;
             
-            if(sum >= 10){
+            if(curr >= 10){
                 ++carry;
-                sum %= 10;
+                curr -= 10;
             }
             
-            tmp.push_back('0' + sum);
+            tmp.push_back('0' + curr);
         }
         
         if(carry){
@@ -44,9 +47,8 @@ string my2Pow(int N){
         res = tmp;
     }
     
+    // 리버스된 res에 -1
     int len = (int)res.size();
-    int curr;
-    
     carry = 0;
     for(int i=0; i<len; i++){
         curr = res[i] - '0' + carry - 1;
@@ -69,6 +71,7 @@ string my2Pow(int N){
 }
 
 int main(){
+    ios_base::sync_with_stdio(false);
     cin.tie(nullptr);
     
     int N;
