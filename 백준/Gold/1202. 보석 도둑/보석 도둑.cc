5a8730ff358e3e447a8ed4c0d035cdc81eb9c127
DABIN LEE <81626630+dabinchiii@users.noreply.github.com>
2026-03-01 13:12:09 +0900

    [Gold II] Title: 보석 도둑, Time: 152 ms, Memory: 8740 KB -BaekjoonHub

--- 백준/Gold/1202. 보석 도둑/보석 도둑.cc
+++ 백준/Gold/1202. 보석 도둑/보석 도둑.cc
@@ -10,25 +10,21 @@ using namespace std;
 int N, K;
 pair<int, int> gem[MAX_N];
 int bag[MAX_K];
-bool isTaken[MAX_N];
 long long ans;
-
-bool compare(pair<int, int> &a, pair<int, int> &b){
-    return a.first < b.first;
-}
 
 void solve(){
     sort(bag, bag + K); // 오름차순
-    sort(gem, gem + N, compare); // 보석 무게 기준 내림차순
+    sort(gem, gem + N); // 보석 무게 기준 내림차순
     
     priority_queue<int> pq;
     
     int idx = 0;
     for(int i=0; i<K; i++){
         while(idx < N && gem[idx].first <= bag[i]) pq.push(gem[idx++].second);
-        if(pq.empty()) continue;
-        ans += pq.top();
-        pq.pop();
+        if(!pq.empty()){
+            ans += pq.top();
+            pq.pop();
+        }
     }
     
     return;
