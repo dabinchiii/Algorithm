ae939daf99d4477aed624e9612f3309ed23d4c33
DABIN LEE <81626630+dabinchiii@users.noreply.github.com>
2026-03-01 22:56:15 +0900

    [Platinum V] Title: 오아시스 재결합, Time: 64 ms, Memory: 6300 KB -BaekjoonHub

--- 백준/Platinum/3015. 오아시스 재결합/오아시스 재결합.cc
+++ 백준/Platinum/3015. 오아시스 재결합/오아시스 재결합.cc
@@ -4,7 +4,7 @@
 using namespace std;
 
 int N;
-unsigned long long ans;
+long long ans;
 
 int main(){
     ios_base::sync_with_stdio(false);
@@ -13,35 +13,29 @@ int main(){
     cin >> N;
     
     stack<pair<int, int>> st; // {num, cnt}
-    int total = 0;
-    int curr;
+    int total = 0, curr;
     for(int i=0; i<N; i++){
         cin >> curr;
         
-        while(!st.empty() && st.top().first < curr){
-            int cnt = st.top().second;
-            ans += cnt;
-            
+        while(!st.empty() && st.top().first < curr){ // 나보다 작은 애들은 내가 들어옴으로써 앞으로 선택될 일이 없음
+            ans += st.top().second;
+            total -= st.top().second;
             st.pop();
-            total -= cnt;
         }
         
-        if(!st.empty() && st.top().first == curr){
-            int cnt = st.top().second;
-            ans += cnt;
+        if(!st.empty() && st.top().first == curr){ // 스택에서 가장 키가 작은 사람들이 나와 같다
+            ans += st.top().second; // 나와 키가 같은 애들은 다 마주볼 수 있다
             
-            st.pop();
-            if(!st.empty()) ++ans;
-            
-            st.push({curr, cnt + 1});
+            if(st.size() > 1) ++ans; // 나보다 큰 사람이 있다 => 마지막 한 명만 마주볼 수 있다
+    
+            st.top().second++;
             ++total;
             
             continue;
         }
         
-        if(!st.empty()){
-            ++ans;
-        }
+        // <- 스택에 나보다 큰 사람만 있다
+        if(!st.empty()) ++ans; // 마지막 한 명만 마주볼 수 있다
         
         st.push({curr, 1});
         ++total;
