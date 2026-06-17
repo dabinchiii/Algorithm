7774dae930913aa0380367dcca9c3122629b25b8
DABIN LEE <81626630+dabinchiii@users.noreply.github.com>
2026-06-17 19:55:28 +0900

    Update 이진 변환 반복하기.cpp

--- 프로그래머스/2/70129. 이진 변환 반복하기/이진 변환 반복하기.cpp
+++ 프로그래머스/2/70129. 이진 변환 반복하기/이진 변환 반복하기.cpp
@@ -8,20 +8,18 @@ int cntZero(string str){
     for(char ch : str) if(ch == '0') ++cnt;
     return cnt;
 }
-string toReverseBinaryString(int num){
-    string result = "";
-    for(;num;num>>=1) result.push_back((num & 1) ? '1' : '0');
-    return result;
+void convert(int &len, int &zero){
+    int num = len - zero;
+    for(len=0, zero=0;num;num>>=1, ++len) if(!(num & 1)) ++zero;
 }
 vector<int> solution(string s) {
     vector<int> answer(2, 0);
     
-    while(s.length() > 1){
-        int zero = cntZero(s);
-        s = toReverseBinaryString(s.length() - zero);
-        
+    int len = s.length(), zero = cntZero(s);
+    while(len > 1){
         ++answer[0];
         answer[1] += zero;
+        convert(len, zero);
     }
     
     return answer;
