c5d9697eb9fcf1d9d7f6cccb0d5db57a0ed10c04
DABIN LEE <81626630+dabinchiii@users.noreply.github.com>
2026-07-02 20:45:41 +0900

    문자열로 하드코딩된 기준일을 DATE 타입으로 변환하도록 수정

--- 프로그래머스/3/131113. 조건별로 분류하여 주문상태 출력하기/조건별로 분류하여 주문상태 출력하기.sql
+++ 프로그래머스/3/131113. 조건별로 분류하여 주문상태 출력하기/조건별로 분류하여 주문상태 출력하기.sql
@@ -9,7 +9,7 @@ SELECT
     OUT_DATE,
     CASE
         WHEN OUT_DATE IS NULL THEN '출고미정'
-        WHEN OUT_DATE <= '2022-05-01' THEN '출고완료'
+        WHEN OUT_DATE <= DATE('2022-05-01') THEN '출고완료'
         ELSE '출고대기'
-    END AS 출고여부
+    END AS '출고여부'
 FROM FOOD_ORDER;
