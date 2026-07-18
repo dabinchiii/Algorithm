1f7775bbfa46effc043dcd985f16c364b1220887
DABIN LEE <81626630+dabinchiii@users.noreply.github.com>
2026-07-19 01:34:47 +0900

    [level 4] Title: 주문량이 많은 아이스크림들 조회하기, Time: , Memory: undefined -BaekjoonHub

---
+++ 프로그래머스/4/133027. 주문량이 많은 아이스크림들 조회하기/주문량이 많은 아이스크림들 조회하기.sql
@@ -0,0 +1,14 @@
+# 7월 아이스크림 총 주문량과 상반기의 아이스크림 총 주문량을 더한 값이 큰 순서대로 상위 3개의 맛을 조회
+WITH TEMP AS (
+    SELECT FLAVOR, TOTAL_ORDER
+    FROM JULY
+    UNION
+    SELECT FLAVOR, TOTAL_ORDER
+    FROM FIRST_HALF
+)
+SELECT
+    FLAVOR
+FROM TEMP
+GROUP BY FLAVOR
+ORDER BY SUM(TOTAL_ORDER) DESC
+LIMIT 3;
